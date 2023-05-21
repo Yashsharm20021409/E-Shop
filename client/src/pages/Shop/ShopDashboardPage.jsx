0d418c9ad38abaac9d3654adfd60cbfde9a1f2c0
Yash Sharma <yashsharma_20021409.cse@geu.ac.in>
2023-05-21 12:09:40 +0530

    created shop dashboard page

--- client/src/pages/Shop/ShopDashboardPage.jsx
+++ client/src/pages/Shop/ShopDashboardPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
 import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
+import DashboardHero from "../../components/Shop/DashboardHero";
 
 const ShopDashboardPage = () => {
   return (
@@ -10,6 +11,7 @@ const ShopDashboardPage = () => {
             <div className="w-[330px] max-md:w-[80px]">
               <DashboardSideBar active={1} />
             </div>
+            <DashboardHero />
           </div>
           {/* dashboardPage */}
         </div>
